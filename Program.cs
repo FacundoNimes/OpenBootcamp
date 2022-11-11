d3ad7a563c0207ac5dcfe672c82c0f079dd90935
Luis Facundo Nimes <facunimes@gmail.com>
2022-11-10 21:37:23 -0300

    second commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//EJERCICIO 1
+Console.WriteLine("Introduce tu nombre: ");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("oliiiiiiiiiiiiiiiiiiiiiiiiiiiiii "+ nombre +"!!" );
+
+//EJERCICIO 2
+Console.WriteLine("Introduce la hora: ");
+string hora = Console.ReadLine();
+
+Console.WriteLine("La hora es: "+ hora);
+
